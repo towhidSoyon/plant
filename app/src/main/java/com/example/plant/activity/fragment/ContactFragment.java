6f2a8f7e7744796737d919a14cc29494ed7f5b8d
towhid <towhidofficial692@gmail.com>
2022-03-09 22:00:16 +0600

    Contact Section Added

--- app/src/main/java/com/example/plant/activity/fragment/ContactFragment.java
+++ app/src/main/java/com/example/plant/activity/fragment/ContactFragment.java
@@ -3,64 +3,83 @@ package com.example.plant.activity.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.plant.R;
+import com.example.plant.activity.adapter.ContactAdapter;
+import com.example.plant.activity.model.ContactInfo;
+import com.example.plant.activity.utilities.Constants;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class ContactFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView contactRecyclerView;
+    
+    FirebaseFirestore firebaseFirestore;
 
     public ContactFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContactFragment newInstance(String param1, String param2) {
-        ContactFragment fragment = new ContactFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view=  inflater.inflate(R.layout.fragment_contact, container, false);
+        
+        contactRecyclerView = view.findViewById(R.id.contactRecyclerView);
+        
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        
+        getContacts();
+        
+        return view;
+    }
+
+    private void getContacts() {
+
+        firebaseFirestore.collection(Constants.KEY_CONTACTS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null){
+                        List<ContactInfo> contactInfoList=new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            ContactInfo contacts=new ContactInfo();
+                            contacts.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            contacts.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            contacts.phoneNumber = queryDocumentSnapshot.getString(Constants.KEY_PHONE_NUMBER);
+                            contacts.district = queryDocumentSnapshot.getString(Constants.KEY_DISTRICT);
+                            contactInfoList.add(contacts);
+                        }
+                        if (contactInfoList.size()>0){
+                            ContactAdapter adapter=new ContactAdapter(contactInfoList);
+                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                            contactRecyclerView.setAdapter(adapter);
+                            contactRecyclerView.setLayoutManager(linearLayoutManager);
+                        }
+                        else {
+                            showToast("Empty List");
+                        }
+                    }
+                    else {
+                        showToast(task.getException().getMessage().toString());
+                    }
+                });
+    }
+
+    public void showToast(String Message){
+        Toast.makeText(getContext(), Message, Toast.LENGTH_SHORT).show();
     }
 }
